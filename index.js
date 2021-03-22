d8d01feb2a517bc6da15093cbfb839415ee4f00b
GalbaKor <cameron.tebbenham@outlook.com>
2021-03-22 15:35:18 +0000

    tabulators with active page working
    tabulators with active page working

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //code learnt from https://www.w3schools.com/w3css/w3css_tabulators.asp accessed 22/03/2021
-function openTopic(topicName) {
-    var i;
-    var x = document.getElementsByClassName("topic");
+function openTopic(evt, topicName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("topic");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
+      x[i].style.display = "none";
     }
-    document.getElementById(topicName).style.display = "block";  
+    tablinks = document.getElementsByClassName("button");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" activeButton", "");
+    }
+    document.getElementById(topicName).style.display = "grid";
+    evt.currentTarget.className += " activeButton";
   }
+   // needed to add style="display-none" to all but the home topic to prevent overlapping
